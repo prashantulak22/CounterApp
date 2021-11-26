82abb3223a6d755c289b15b5908d40f1ad2dea9f
Prashant <prashant9843215397@gmail.com>
2021-11-26 15:43:07 +0545

    Counter app completed

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { numDecrease, numIncrease } from './Actions/Action';
 
 const App = () => {
+  const myState = useSelector(state => state.counterNumber);
+  const dispatch = useDispatch()
   return (
     <div className="container">
       <h1>Counter App with React and Redux</h1>
       <div className="content">
-        <a className="square_btn">Minus</a>
-        <input  className="input-area" type="text" value="0" />
-        <a  className="square_btn">Plus</a>
+        <a className="square_btn" onClick={() => dispatch(numDecrease())}>Minus</a>
+        <input className="input-area" type="text" value={myState} />
+        <a className="square_btn" onClick={() => dispatch(numIncrease())}>Plus</a>
       </div>
     </div>
   )
